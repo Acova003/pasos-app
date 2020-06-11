706e06557ff6d8b937aecfcd51185a19472167bb
Amee <ameecova@yahoo.com>
2020-06-10 22:56:17 -0700

    setting up google oauth

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 """Backend Flask Server for Pasos app."""
 
-from flask import (Flask, request, flash, session, redirect, render_template)
+from flask import (Flask, request, flash, session, redirect, render_template, url_for)
 from model import Trip, db, connect_to_db
+from flask_dance.contrib.google import make_google_blueprint, google
+from werkzeug.contrib.fixers import ProxyFix
 # from flask_oauth import OAuth
 import crud
 
@@ -10,12 +12,28 @@ load_dotenv()
 
 app = Flask(__name__)
 connect_to_db(app)
+app.wsgi_app = ProxyFix(app.wsgi_app)
+app.secret_key = SECRET_KEY
+blueprint = make_google_blueprint(
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    scope=["profile", "email"]
+)
+app.register_blueprint(blueprint, url_prefix="/login")
 
 @app.route("/")
 def index():
-    trips = [Trip(title='123')]
-    # trips = crud.get_trips()
-    return render_template('index.html', trips=trips)
+    if not google.authorized:
+        return redirect(url_for("google.login"))
+    resp = google.get("/plus/v1/people/me")
+    assert resp.ok, resp.text
+    return "You are {email} on Google".format(email=resp.json()["emails"][0]["value"])
+
+# @app.route("/")
+# def index():
+#     trips = [Trip(title='123')]
+#     # trips = crud.get_trips()
+#     return render_template('index.html', trips=trips)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
